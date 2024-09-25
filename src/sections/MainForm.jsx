b2c279000570181c22aca805776f4e26393b0961
ikwerre-dev <investorhonour@gmail.com>
2024-09-26 00:04:35 +0100

    Initial commit

---
+++ src/sections/MainForm.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-white  p-4 
+                      w-full h-[45rem] md:w-[30rem] mx-auto 
+                      mt-20 rounded-[2rem] shadow-xl">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
